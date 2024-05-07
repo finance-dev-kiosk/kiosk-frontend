d23a70a661d93fee178e49a29c8c2c8b12f8d671
pocj8ur4in <pocj8ur4in@gmail.com>
2024-05-07 21:42:32 +0900

    init: 프로젝트 세팅
    # next.js 기반으로 프로젝트를 생성합니다.
    # 스타일 유틸리티로 tailwindcss를 프로젝트에 적용합니다.
    # 테스팅 라이브리러리로 jest를 프로젝트에 적용합니다.
    # eslint와 prettier 간 충돌 문제를 해결합니다.

---
+++ apps/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: '성수맛집',
+  description: '성수의 맛집을 소개합니다.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='ko'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
